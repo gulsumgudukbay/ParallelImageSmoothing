34011e35c8d21a3fb6a9ef138e18f50317fabe20
Gulsum <gulsum@Gulsums-MacBook-Pro.local>
2017-10-17 15:13:05 +0300

    Initial Commit

---
+++ mpi_hello/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  mpi_hello
+//
+//  Created by Gulsum on 17/10/2017.
+//  Copyright © 2017 Gulsum. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
